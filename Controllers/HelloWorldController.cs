dc78faf96a15c88fb5943556e857710cd883cb7e
Darshen Nadar <166602264+darshen-nadar@users.noreply.github.com>
2025-02-20 22:09:21 +0200

    Add files via upload

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        // GET: HelloWorld
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        //GET: /HelloWorld/Welcome//
+        //Requires using Systems.Text.Encodings.Web 
+
+        public string Welcome(string name, int numTimes = 1) 
+        {
+           return HtmlEncoder.Default.Encode($"Hello {name}, numofTimes is {numTimes}");
+
+        }
+
+
+    }
+}
